8446685afb5ff7e8b4fb7cc57e78d16e5db8f30d
Md Bocktear Abid Borat <boratabid@gmail.com>
2025-09-14 00:43:48 +0600

    commit-2

--- price-option/src/App.jsx
+++ price-option/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navber from './Navber'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,6 +23,7 @@ function App() {
     </div>
   </div>
 </div>
+ <Navber></Navber>
     </>
   )
 }
